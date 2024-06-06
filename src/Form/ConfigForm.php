73d6f78a055ed512030e8d440234c07e956bf46d
Luis Royo Antín <luisroyozgz@gmail.com>
2024-06-06 19:25:51 +0200

    Add config label

--- src/Form/ConfigForm.php
+++ src/Form/ConfigForm.php
@@ -7,6 +7,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\action_links_creator\Service\ContentTypeService;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Defines a form to configure action links.
@@ -28,16 +29,26 @@ class ConfigForm extends ConfigFormBase {
   protected $contentTypeService;
 
   /**
+   * The request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
    * Constructs a new ConfigForm object.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
    * @param \Drupal\action_links_creator\Service\ContentTypeService $content_type_service
    *   The content type service.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, ContentTypeService $content_type_service) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, ContentTypeService $content_type_service, RequestStack $request_stack) {
     $this->entityTypeManager = $entity_type_manager;
     $this->contentTypeService = $content_type_service;
+    $this->requestStack = $request_stack;
   }
 
   /**
@@ -46,7 +57,8 @@ class ConfigForm extends ConfigFormBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('entity_type.manager'),
-      $container->get('action_links_creator.content_type_service')
+      $container->get('action_links_creator.content_type_service'),
+      $container->get('request_stack')
     );
   }
 
@@ -71,8 +83,14 @@ class ConfigForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $content_types = $this->contentTypeService->getAllContentTypes();
-
     $config = $this->config('action_links_creator.configuration');
+    $admin_content_url = $this->requestStack->getCurrentRequest()->getHost() . '/admin/content';
+
+    $form['label'] = [
+      '#type' => 'item',
+      '#title' => $this->t('Content types with action links to create nodes'),
+      '#markup' => $this->t('If they are active, they will be visible <a href="https://'. $admin_content_url . '">here</a>.')
+    ];
       
     foreach ($content_types as $content_type => $content_type_label) {
       $form['checkbox_' . $content_type] = [
@@ -92,9 +110,8 @@ class ConfigForm extends ConfigFormBase {
     parent::submitForm($form, $form_state);
     
     $config = $this->config('action_links_creator.configuration');
-
+    
     $checkboxes = $form_state->getValues();
-
     foreach ($checkboxes as $checkbox => $value) {
       $config->set($checkbox, $value);
     }
